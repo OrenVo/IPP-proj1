85fac7873f5e236d55a7024aefa20c477ad3af65
Vojtěch Ulej <xulejv00@stud.fit.vutbr.cz>
2020-02-08 23:58:41 +0100

    Class for scanner created

--- parser/parse.php
+++ parser/parse.php
@@ -1,9 +1,34 @@
 <?php
-if ($argc == 2 && $argv[1] == "--help") {
-  printf("Skript parse.php načítá vstup ze standardního vstupu a výstup vypisuje na standardní výstup.\nNa vstupu očekává zdrojový kód napsaný v jazyce IPPcode20.
-Na výstup vypíše reprezentaci programu ve formátu XML.\n");
+/************************************************************
+ * @file parse.php                                          *
+ * @author Vojtěch Ulej (xulejv00)                          *
+ * @brief IPP projekt 1                                     *
+ * Skript který provede lexikální a syntakticou analýzu.    *
+ ************************************************************/
+declare(strict_types = 1); // Pro jistotu zapnout datovou kontrolu :D
+function parse_args(){
+ if ($argc == 2 && $argv[1] == "--help") {
+ printf(
+"php parse.php [--help]
+--help - Zobrazí tuto nápovědu.
+Skript parse.php načítá vstup ze standardního vstupu a výstup vypisuje na standardní výstup.
+Na vstupu očekává zdrojový kód napsaný v jazyce IPPcode20.
+Poté provede lexikální a syntaktickou analýzu. Pokud vše proběhne bez chyby. Na výstup vypíše reprezentaci programu ve formátu XML.
+Chybové a ladící hlášky sktript vypisuje na standardní chybový výstup.
+");
+ exit(0);
+ } elseif ($argc > 1) {
+   fwrite(STDERR,"Neplatný počet argumentů: $argc\nNebo neznámé argumenty: ");
+   foreach ($argv as $arg) {
+     if ($arg == basename(__FILE__)) { // Odstraní $argv[0] (název skriptu)
+       continue;
+     }
+     fwrite(STDERR,$arg); fwrite(STDERR," ");
+   }
+   fwrite(STDERR,"\n");
+   exit(10);
+ }
 }
-
 /** Definice konstant tokenů **/
 // INSTRUKCE
   const  TOKEN_MOVE = 0;
@@ -44,16 +69,131 @@ Na výstup vypíše reprezentaci programu ve formátu XML.\n");
 // Ostatní klíčová slova + znaky
   const  TOKEN_EOL = 35;
   const  TOKEN_EOF = 36;
-  const  TOKEN_IPPcode20 = 37;
-  const  TOKEN_AT = 38;
+  const  TOKEN_IPPcode20 = 37;    // hlavička .IPPcode20
+  const  TOKEN_AT = 38;           // @
 // Paměťové rámce
   const  TOKEN_GF = 39;
-  const  TOKEN_GF = 40;
-  const  TOKEN_GF = 41;
+  const  TOKEN_TF = 40;
+  const  TOKEN_LF = 41;
 // Datové typy
   const TOKEN_bool = 42;
   const TOKEN_nil = 43;
   const TOKEN_string = 44;
   const TOKEN_int = 45;
+
+$KeyWords = array(
+  TOKEN_MOVE         => "MOVE",
+  TOKEN_CREATEFRAME  => "CREATEFRAME",
+  TOKEN_PUSHFRAME    => "PUSHFRAME",
+  TOKEN_POPFRAME     => "POPFRAME",
+  TOKEN_DEFVAR       => "DEFVAR",
+  TOKEN_CALL         => "CALL",
+  TOKEN_RETURN       => "RETURN",
+  TOKEN_PUSHS        => "PUSHS",
+  TOKEN_POPS         => "POPS",
+  TOKEN_ADD          => "ADD",
+  TOKEN_SUB          => "SUB",
+  TOKEN_MUL          => "MUL",
+  TOKEN_IDIV         => "IDIV",
+  TOKEN_LT           => "LT",
+  TOKEN_GT           => "GT",
+  TOKEN_EQ           => "EQ",
+  TOKEN_AND          => "AND",
+  TOKEN_OR           => "OR",
+  TOKEN_NOT          => "NOT",
+  TOKEN_INT2CHAR     => "INT2CHAR",
+  TOKEN_STRI2INT     => "STRI2INT",
+  TOKEN_READ         => "READ",
+  TOKEN_WRITE        => "WRITE",
+  TOKEN_CONCAT       => "CONCAT",
+  TOKEN_STRLEN       => "STRLEN",
+  TOKEN_GETCHAR      => "GETCHAR",
+  TOKEN_SETCHAR      => "SETCHAR",
+  TOKEN_TYPE         => "TYPE",
+  TOKEN_LABEL        => "LABEL",
+  TOKEN_JUMP         => "JUMP",
+  TOKEN_JUMPIFEQ     => "JUMPIFEQ",
+  TOKEN_JUMPIFNEQ    => "JUMPIFNEQ",
+  TOKEN_EXIT         => "EXIT",
+  TOKEN_DPRINT       => "DPRINT",
+  TOKEN_BREAK        => "BREAK"
+);
+
+/**
+ * Třída tokenu
+ */
+class Token
+{
+  public $type;
+  public $attr;
+  function __construct($type, $attr)
+  {
+    $this->$type = $type;
+    $this->$attr = $attr;
+  }
+}
+
+/**
+ *  Třída mé vlastní vyjímky
+ */
+class my_Exception extends Exception
+{
+  public $err_message;
+
+  public $err_code;
+
+  function __construct(string $err_message,int $err_code)
+  {
+    $this->err_message = $err_message;
+    $this->err_code = $err_code;
+  }
+
+  public function last_words(){
+    fwrite(STDERR,"Chyba: " . $this->err_message . "\n");
+    die($this->err_code);
+  }
+}
+
+function get_input(){
+  $in = fopen( 'php://stdin', 'r' );
+  if ($in == FALSE) { // Nepovedlo se otevřít stdin
+    var_dump($in);
+    throw new my_Exception("Nelze otevřít vstupní soubor.", 11);
+  } else{
+      $input = file_get_content($in);
+      if ($input == FALSE){
+          var_dump($input);
+          throw new my_Exception("Chyba při načítání vstupu.", 99);
+      }
+      else
+        return $input;
+  }
+}
+
+/**
+ * Třída reprezentuje lexikální analyzátor. Načítá vstup z stdin, poté provede lex analýzu a generuje příslušné tokeny.
+ */
+class Scanner
+{
+  private $line_num = 1;
+  public $input;
+  private $index = 11;
+  function __construct($input){
+      $this->input = $input;
+  }
+  // Funkce zkontroluje přítomnost hlavičky, poté zahájí lexikální analýzu
+  public function scan(){
+      if (substr($this->input,0,12) <> ".IPPcode20\n") {
+        throw new my_Exception("Na řádku $this->line_num chybí hlavička", 21);
+      }
+      Final_State_Machine();
+  }
+
+  private function Final_State_Machine(){
+
+  }
+
+}
+
 
 ?>
